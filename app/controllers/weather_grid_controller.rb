a1555a3b10861226324267a9da1ae4c41f66938a
shibabu <shibabu.alfa@gmail.com>
2016-09-14 16:08:52 +0900

    one more C

---
+++ app/controllers/weather_grid_controller.rb
@@ -0,0 +1,5 @@
+class WeatherGridController < ApplicationController
+  def index
+    #TODO
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   root to: "home#index"
+
+  resources :weather_grid
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
