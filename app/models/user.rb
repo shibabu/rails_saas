65fa40974ea41e476695d1736b1e8f19607fa70a
shibabu <shibabu.alfa@gmail.com>
2016-09-18 20:02:23 +0900

    SaaS Accounts

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # validate :email_is_unique, on: :create
+  after_create :create_account
+
+  private
+
+  def create_account
+    acoount = Account.new email: email
+    account.save!
+  end
+
+  # Make sure the email is a unique ID
+  # def email_is_unique
+  #   unless Account.find_by_email(email).nil?
+  #     errors.add :email, ' is already used!'
+  #   end
+  # end
+
 end
